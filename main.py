4cd68dba4d666a12717542346f07869e51f5c3c2
dead inside <90174775+xxxvvvxxxvx@users.noreply.github.com>
2021-10-04 06:55:36 +0000

    qt

--- main.py
+++ main.py
@@ -2,5 +2,17 @@
 Ēriks Mačulis
 04.10
 '''
-def pap_a ():
-  print('aaa')
+def gg_wp():
+  print('pppp')
+
+def gg_wp (pp):
+  print(pppp, pp)
+
+def gg_wp (pp, qq):
+  print(pppp, pp, qq)
+
+def gg_wp (pp, qq):
+  return pp + qq
+rrr = ww
+rrr = ww
+print(pppp (rrr, rrr))
